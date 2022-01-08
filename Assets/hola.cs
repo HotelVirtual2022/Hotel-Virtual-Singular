19590d7794e83632c30d8235e42adbf38302092e
Diego Solar <dieg.solar@duocuc.cl>
2022-01-08 20:50:35 -0300

    Creacion de Script Hola.cs

---
+++ Assets/hola.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hola : MonoBehaviour
+{
+    void Start(){
+        Debug.log("HOLAAAAAAA");
+    }
+
+
+}
